b576ac12a6d10edd434b52741d4ba5da18977bf5
Mark Joseph Livado <livadomc@gmail.com>
2025-09-11 22:43:11 +0800

    update: dependencies to latest ver. of sdk for expo go

--- LakbAI-Mobile/config/apiConfig.ts
+++ LakbAI-Mobile/config/apiConfig.ts
@@ -3,7 +3,7 @@ import Constants from 'expo-constants';
 
 // Network configuration for different developers
 const DEVELOPER_CONFIGS: { [key: string]: string } = {
-  joseph: '192.168.254.104',
+  joseph: '192.168.254.111',
   jiro: '192.168.254.111',  
 };
 
